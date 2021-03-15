e04163649a13b7d5eb8bc071ddfdf1f0d01063b8
GreyWay2 <greenpeesoleg@gmail.com>
2021-03-15 15:33:05 +0400

    feat: add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    var brackets = {};
+    for(var i=0; i<bracketsConfig.length; i+=1) {
+        brackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+    }
+    var stack = [];
+    stack.push(str.charAt(0));
+    for(var i=1; i<str.length; i+=1) {
+      if(str.charAt(i) === brackets[stack.slice(-1)]) {
+        stack.pop();
+      } 
+      else {
+        stack.push(str.charAt(i)); 
+      }
+    }
+    return (stack.length===0)?true:false;
+  };
